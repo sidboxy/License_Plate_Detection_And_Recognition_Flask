8a4253a444173b9c10e205bb9b8f56b5706860df
sidboxy <58976263+sidboxy@users.noreply.github.com>
2020-09-25 15:24:59 +0530

    Update App.py

--- App.py
+++ App.py
@@ -27,8 +27,8 @@ NUM_CLASSES = 1
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    od_graph_def = tf.compat.v1.GraphDef()
+    with tf.compat.v2.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
